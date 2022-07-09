247baecf2bb5726e6487a3b44026b764c6c5386c
Ruihao <105295766+liangshangdejunzi@users.noreply.github.com>
2022-07-09 22:53:05 +0800

    Add files via upload

---
+++ 对任意三个数字进行排序.cpp
@@ -0,0 +1,36 @@
+# include <stdio.h>
+
+int main (void)
+{
+    int a,b,c;//等价于： int a; int b; int c;
+	int t;
+
+	printf("请输入三个整数（中间以空格分隔）;");
+    scanf("%d %d %d",&a,&b,&c);
+    
+	//编写代码完成a是最大值 b是中间值 c是最小值
+    if (a<b)//a小于b才互换，a大于等于b不进行任何操作。
+	{
+        t = a;
+		a = b;
+		b = t;
+	}
+
+	if (a<c)
+	{
+		t = a;
+		a = c;
+		c = t;
+	}
+
+	if (b<c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+
+	printf("%d %d %d\n",a,b,c);
+
+    return 0;
+}
